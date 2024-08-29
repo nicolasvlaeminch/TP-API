174ed39f0fb697e1e5df39036ed2e86d9828aade
nicolasvlaeminch <nicolasvlaeminch777@gmail.com>
2024-08-29 02:57:47 -0300

    init: inicializar proyecto Spring Boot 'Turnos Rotativos'.

---
+++ src/main/java/com/turnosrotativos/TurnosRotativosApplication.java
@@ -0,0 +1,13 @@
+package com.turnosrotativos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurnosRotativosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TurnosRotativosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/turnosrotativos/TurnosRotativosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.turnosrotativos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TurnosRotativosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
